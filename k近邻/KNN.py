daecc7383cd2aacd197a138e00d3936853abf3b1
LAPTOP-NAA0T7MN\Jinbao Cheng <jinbao>
2024-05-29 20:55:30 +0800

    k近邻算法

--- k近邻/KNN.py
+++ k近邻/KNN.py
@@ -4,6 +4,9 @@ import tqdm as tqdm
 
 
 def load_data(file_path):
+    """
+    加载Mnist数据集
+    """
     data = []
     label = []
     with open(file_path, 'r') as f:
@@ -14,14 +17,24 @@ def load_data(file_path):
     return np.array(data), np.array(label)
 
 def EuclideanDistance(x, y):
+    """
+    计算欧几里得距离
+    """
     return np.sqrt(np.sum(np.square(x - y)))
 
 def ManhattanDistance(x, y):
+    """
+    计算曼哈顿距离
+    """
     return np.sqrt(np.sum(np.abs(x - y)))
 
 def getClosest(train_data, train_label, x, topk, method):
+    """
+    根据训练数据得到最近的k个样本，根据样本类别数设定x的类别
+    """
     distance = []
     for sample in train_data:
+        # 两种计算方法，一般选择欧几里得距离
         if method == 'Euclidean':
             distance.append(EuclideanDistance(sample, x))
         else:
@@ -31,6 +44,9 @@ def getClosest(train_data, train_label, x, topk, method):
     return np.argmax(np.bincount(label))
 
 def test(train_data, train_label, test_data, test_label, topk, method):
+    """
+    根据训练数据对测试数据进行测试，得到准确率
+    """
     print('Start testing...')
     correct = 0
     for i in tqdm.tqdm(range(len(test_data))):
@@ -39,14 +55,21 @@ def test(train_data, train_label, test_data, test_label, topk, method):
     return correct / len(test_data)
 
 def main():
+    # 加载训练和测试数据
     print('Start loading training data...')
     train_data, train_label = load_data('../mnist_train.csv')
     print('Start loading testing data...')
     test_data, test_label = load_data('../mnist_test.csv')
+
+    # 为了节约时间，选择前两百个数据进行测试
     test_data = test_data[:200]
     test_label = test_label[:200]
+
+    # 选择了欧几里得距离，k的值设定为25
     topk = 25
     method = 'Euclidean'
+
+    # 打印结果
     start_time = time.time()
     accuracy = test(train_data, train_label, test_data, test_label, topk, method)
     end_time = time.time()
